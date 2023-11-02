b549bba64a4a9340edff7d2dbe2cceaa32020907
nart <nart.janouk@gmail.com>
2023-11-01 22:50:59 -0400

    Add passing tests for the functions

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2; 
 
-// Write your code here
+function generateRandomInteger() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+
+const random = generateRandomInteger(); 
+
+const num3 = 24; 
+const num4 = 5;
+const mod = num3 % num4; 
+
+const numbers = [10, 20, 15];
+const max = Math.max(...numbers); 
+
+
